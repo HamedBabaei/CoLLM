ede1d559068fdb1571b01a58fb22fe2e3a429d61
Hamed Babaei Giglou <hamedbabaeigiglou@gmail.com>
2024-11-06 15:03:08 +0100

    [add] task B and C of LLMs4OL code

---
+++ case-studies/LLMs4OL/task_c_runner.sh
@@ -0,0 +1,18 @@
+!/bin/bash
+model_names=("llama3" "flan_t5_small" "flan_t5_base" "flan_t5_large" "flan_t5_xl" "flan_t5_xxl")
+rqs=("rq3" "rq2" "rq2" "rq2" "rq1" "rq2")
+runs=("r1" "r2" "r3")
+datasets=("umls")
+task="C"
+
+for dataset in "${datasets[@]}"; do
+    for i in "${!model_names[@]}"; do
+        model_name="${model_names[$i]}"
+        rq="${rqs[$i]}"
+        for run_no in "${runs[@]}"; do
+            echo "Running task_b.py with model_name=$model_name, dataset=$dataset, run_no=$run_no, rq=$rq"
+            python task_bc.py --model_name="$model_name" --task="$task" --dataset="$dataset" --run_no="$run_no" --rq="$rq"
+        done
+    done
+done
+
